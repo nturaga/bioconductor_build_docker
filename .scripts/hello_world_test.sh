9c59ff18eee521bc29aee9714b9658c67108b557
Nitesh Turaga <nitesh.turaga@gmail.com>
2020-03-31 08:50:16 -0400

    Add updated scripts

--- .scripts/hello_world_test.sh
+++ .scripts/hello_world_test.sh
@@ -3,15 +3,32 @@ echo "Hello Github actions"
 ## Rocker repo
 ROCKER_REPO=https://github.com/rocker-org/rocker-versioned
 
+BIOCONDUCTOR_REPO=https://github.com/bioconductor/bioconductor_docker
+
 HERE=`pwd`
 
-## git clone rocker repo
+echo $HERE
+
+## git clone rocker and bioc repo
 git clone $ROCKER_REPO
+
+git clone $BIOCONDUCTOR_REPO
 
 ## docker build rocker repo
 
 cd rocker-versioned/r-ver/
 
 docker build -t rocker/r-ver:devel -f  devel.Dockerfile .
+
+cd $GITHUB_WORKSPACE; cd rocker-versioned/rstudio
+
+echo "*** Building rocker/rstudio *** \n"
+
+docker build -t rocker/rstudio:devel -f  devel.Dockerfile .
+
+cd $GITHUB_WORKSPACE; cd bioconductor_docker
+echo "*** Building bioconductor/bioconductor_docker *** \n"
+
+docker build -t bioconductor/bioconducotor_docker:devel .
 
 cd $HERE
