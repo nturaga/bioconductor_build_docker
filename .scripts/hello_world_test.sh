432cebeb7eba48b9bbf102922017c5e25f622b03
Nitesh Turaga <nitesh.turaga@gmail.com>
2020-04-01 10:51:46 -0400

    Final test with version bumps

--- .scripts/hello_world_test.sh
+++ .scripts/hello_world_test.sh
@@ -1,24 +1,26 @@
 echo "Build bioconductor_docker image"
 
+
+###############################################
+## Step 0: Set up variables and clone
 ## Rocker repo
 ROCKER_REPO=https://github.com/rocker-org/rocker-versioned
-
 BIOCONDUCTOR_REPO=https://github.com/bioconductor/bioconductor_docker
-
 HERE=`pwd`
-
-echo $HERE
 
 ## git clone rocker and bioc repo
 git clone --depth 1 $ROCKER_REPO
-
 git clone --depth 1 $BIOCONDUCTOR_REPO
 
-## docker build rocker repo
+###############################################
+## 1. docker build rocker r-ver:devel repo
 
 cd rocker-versioned/r-ver/
 
 docker build -t rocker/r-ver:devel -f  devel.Dockerfile .
+
+###############################################
+## 2. docker build rocker rstudio:devel
 
 cd $GITHUB_WORKSPACE; cd rocker-versioned/rstudio
 
@@ -26,8 +28,14 @@ echo "*** Building rocker/rstudio *** \n"
 
 docker build -t rocker/rstudio:devel -f  devel.Dockerfile .
 
+###############################################
+## 3. docker build bioconductor_docker:devel
+
 cd $GITHUB_WORKSPACE; cd bioconductor_docker
 echo "*** Building bioconductor/bioconductor_docker *** \n"
+
+## increment version number with awk
+awk -F . '/^ARG /{$NF++; OFS="."} {print}'  Dockerfile > tmp && mv -f tmp Dockerfile
 
 docker build -t nitesh1989/bioconductor_docker:devel .
 
@@ -35,4 +43,6 @@ docker login --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD
 
 docker push nitesh1989/bioconductor_docker:devel
 
+## Finish
+echo "Done"
 cd $HERE
